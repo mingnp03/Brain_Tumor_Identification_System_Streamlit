e7f997639316975c73d825d996c7dd42c4727f2c
mingnp03 <159937632+mingnp03@users.noreply.github.com>
2024-06-17 21:02:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ import tensorflow as tf
 from tensorflow.keras.layers import DepthwiseConv2D as OriginalDepthwiseConv2D
 import streamlit as st
 
-import gdown
-url = 'https://drive.google.com/file/d/1LlML6NGwEG8dtjgKoOquxJTCTQ_pYyX2/view?usp=sharing'
-output = 'efficientnetv2-s-BTI44-96.91.h5'
-gdown.download(url, output, quiet=False)
+#import gdown
+#url = 'https://drive.google.com/file/d/1LlML6NGwEG8dtjgKoOquxJTCTQ_pYyX2/view?usp=sharing'
+#output = 'efficientnetv2-s-BTI44-96.91.h5'
+#gdown.download(url, output, quiet=False)
 
 st.set_page_config(page_title="Brain Tumor Identification System", layout="wide")
 st.title("Brain Tumor Identification System")
